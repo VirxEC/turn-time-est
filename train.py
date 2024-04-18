a88b800fa886b98aa23e6fde587bf04dcc306eb1
VirxEC <virx@virxcase.dev>
2024-04-18 13:20:12 -0400

    Model training

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import numpy as np
 import numpy.typing as npt
 import torch
-from tqdm import tqdm
+from tqdm import trange
 
 
 def load_data_from_file(file_name: Path) -> npt.NDArray:
@@ -13,37 +13,29 @@ def load_data_from_file(file_name: Path) -> npt.NDArray:
         return np.fromfile(f, dtype=np.float32).reshape(-1, 7)
 
 
-def get_data(data_folder: Path) -> npt.NDArray:
-    data_file_names = list(data_folder.glob("*.bin"))[:3]
-
-    print(f"Found {len(data_file_names)} data files, loading...")
-    file_data = [load_data_from_file(file_name) for file_name in tqdm(data_file_names)]
-
-    # combine into one array
-    data = np.concatenate(file_data, axis=0)
-
-    # delete to free up ram
-    del file_data
-
-    print(f"Loaded {len(data):,} data points")
-
-    return data
-
-
 def split_train_test(
     data: npt.NDArray, test_train_split: float
 ) -> tuple[tuple[torch.Tensor, torch.Tensor], tuple[torch.Tensor, torch.Tensor]]:
     num_data = len(data)
     num_train = int(num_data * test_train_split)
 
-    # first 6 are inputs, last one is output (time)
+    # first 3 inputs are in radians per second (doesn't exceed +/- 5.5)
+    # it's the initial angular velocity of the object, relative to the object
+    # next 3 inputs are in radians (doesn't exceed +/- PI)
+    # it's the initial target of the object, relative to the object
+    # last float is in seconds (any positive value)
+    # this is the time it took for the object to face the target
 
-    x_train = torch.from_numpy(data[:num_train, :6])
-    y_train = torch.from_numpy(data[:num_train, 6])
+    norm_data = data / np.array([5.5, 5.5, 5.5, np.pi, np.pi, np.pi, 1.], dtype=np.float32)
+    del data
 
-    x_test = torch.from_numpy(data[num_train:, :6])
-    y_test = torch.from_numpy(data[num_train:, 6])
+    x_train = torch.from_numpy(norm_data[:num_train, :6])
+    y_train = torch.from_numpy(norm_data[:num_train, 6])
 
+    x_test = torch.from_numpy(norm_data[num_train:, :6])
+    y_test = torch.from_numpy(norm_data[num_train:, 6])
+
+    del norm_data
     print(f"Train: {len(x_train):,}, Test: {len(x_test):,}")
 
     return (x_train, y_train), (x_test, y_test)
@@ -51,11 +43,68 @@ def split_train_test(
 
 if __name__ == "__main__":
     data_folder = Path("../stat-final-data/results/").resolve()
+    data_file_names = list(data_folder.glob("*.bin"))
+    num_files = len(data_file_names)
 
-    data = get_data(data_folder)
-    (x_train, y_train), (x_test, y_test) = split_train_test(data, 0.85)
+    model = torch.nn.Sequential(
+        torch.nn.Linear(6, 128),
+        torch.nn.Sigmoid(),
+        torch.nn.Linear(128, 128),
+        torch.nn.Sigmoid(),
+        torch.nn.Linear(128, 1),
+        torch.nn.ReLU(),
+    )
 
-    # delete to free up ram
-    del data
+    loss_fn = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-    input()
+    for i, file_name in enumerate(data_file_names):
+        print(f"\nTraining on {file_name} ({i+1}/{num_files})")
+        data = load_data_from_file(file_name)
+        (x_train, y_train), (x_test, y_test) = split_train_test(data, 0.9)
+
+        num_data = len(x_train)
+        data_per_epoch = 100_000
+        num_epochs = num_data // data_per_epoch
+
+        for epoch in trange(num_epochs):
+            x_train_epoch = x_train[
+                epoch * data_per_epoch : (epoch + 1) * data_per_epoch
+            ]
+            y_train_epoch = y_train[
+                epoch * data_per_epoch : (epoch + 1) * data_per_epoch
+            ]
+
+            y_pred = model(x_train_epoch)
+            del x_train_epoch
+
+            y_pred = y_pred.view(-1)
+
+            loss = loss_fn(y_pred, y_train_epoch)
+            del y_train_epoch
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        del x_train, y_train
+
+        torch.save(model.state_dict(), "model.pth")
+
+        model.eval()
+        y_pred = model(x_test)
+        model.train()
+
+        y_pred = y_pred.view(-1)
+
+        test_loss = loss_fn(y_pred, y_test)
+
+        # loss
+        loss = test_loss.item()
+        print(f"Test loss: {loss}")
+
+        # accuracy
+        accuracy = torch.mean(torch.abs(y_pred - y_test)).item()
+        print(f"Accuracy: {accuracy}")
+
+        del x_test, y_test
